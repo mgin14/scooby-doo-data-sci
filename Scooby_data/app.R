69e4135deceb19baa0bbd54b137bce5586797833
Melanie Gin <mgin14@email.arizona.edu>
2022-04-28 01:50:05 -0700

    Tidied up scooby data

---
+++ Scooby_data/app.R
@@ -0,0 +1,106 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(tidyr)
+library(lubridate)
+
+# Get and read in data (code provided by creator for the TidyTuesday challenge)
+# Or read in the data manually
+scoobydoo <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-13/scoobydoo.csv')
+
+# Pivot longer to condense caught_[character] in order to see who caught the
+# culprit in that episode
+scoobydoo_tidy <- scoobydoo %>% 
+  pivot_longer(cols = caught_fred:caught_scooby,
+               names_to = "caught_by",
+               values_to = "were_caught") %>% 
+  filter(!is.na(were_caught))
+
+# remove rows where the character does not catch the culprit
+scoobydoo_tidy <- subset(scoobydoo_tidy, were_caught==TRUE)
+
+# replace value of caught_by with only the name of the character
+scoobydoo_tidy <- scoobydoo_tidy %>% 
+  mutate(
+    caught_by = str_match(caught_by, pattern = "fred|daphnie|velma|shaggy|scooby")
+  )
+
+scoobydoo_tidy <- scoobydoo_tidy %>% 
+  mutate(
+    date_aired = year(date_aired)
+  )
+  
+# create a list of character names from data
+character_options <- scoobydoo_tidy %>% 
+  filter(!is.na(caught_by)) %>% 
+  distinct(caught_by)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Scooby-Doo Series Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            h4("Select data")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          tabsetPanel(type = "tabs",
+                      tabPanel("Distribution",
+                               selectInput("character_to_highlight",
+                                           "Choose character to highlight:",
+                                           choices = character_options),
+                               plotOutput("distPlot",
+                                          height = "800px")
+                      )
+            
+          )
+           
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    #Who has caught the culprit the most throughout the series?
+    
+  
+    output$distPlot <- renderPlot({
+       all_series <-  scoobydoo_tidy %>% 
+          filter(format == "TV Series") %>%
+          group_by(series_name, date_aired) %>%
+          count(caught_by)
+      
+        highlighted_character <- all_series %>% 
+          filter(caught_by == input$character_to_highlight)
+      
+        all_series %>% 
+          ggplot(aes(x = series_name,
+                     y = n,
+                     group = caught_by),
+                 srt = 35) +
+          geom_line(color = "grey") +
+          geom_line(data = highlighted_character,
+                    color = "red") +
+          geom_point(color = "grey")+
+          geom_point(data = highlighted_character,
+                     color = "red") +
+          theme(axis.text.x = element_text(angle = 45, hjust=1))
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
