5850d5fc870f877259805a5d842419ec578db58c
Melanie Gin <mgin14@email.arizona.edu>
2022-04-29 00:59:15 -0700

    Finished line graph to answer frew, created second

--- Scooby_data/app.R
+++ Scooby_data/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(tidyverse)
 library(tidyr)
 library(lubridate)
+library(RColorBrewer)
 
 # Get and read in data (code provided by creator for the TidyTuesday challenge)
 # Or read in the data manually
@@ -29,14 +30,10 @@ scoobydoo_tidy <- subset(scoobydoo_tidy, were_caught==TRUE)
 
 # replace value of caught_by with only the name of the character
 scoobydoo_tidy <- scoobydoo_tidy %>% 
-  mutate(
-    caught_by = str_match(caught_by, pattern = "fred|daphnie|velma|shaggy|scooby")
-  )
+  mutate(caught_by = str_match(caught_by, pattern = "fred|daphnie|velma|shaggy|scooby"))
 
 scoobydoo_tidy <- scoobydoo_tidy %>% 
-  mutate(
-    date_aired = year(date_aired)
-  )
+  mutate(date_aired = year(date_aired))
   
 # create a list of character names from data
 character_options <- scoobydoo_tidy %>% 
@@ -52,7 +49,7 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            h4("Select data")
+            h4("Select tab to show what data to display")
         ),
 
         # Show a plot of the generated distribution
@@ -63,8 +60,10 @@ ui <- fluidPage(
                                            "Choose character to highlight:",
                                            choices = character_options),
                                plotOutput("distPlot",
-                                          height = "800px")
-                      )
+                                          height = "800px")),
+                      tabPanel("Barplot",
+                               plotOutput("bar_plot",
+                                                     height = "800px"))
             
           )
            
@@ -75,21 +74,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    #Who has caught the culprit the most throughout the series?
-    
-  
+    # Who has caught the culprit the most per series? How has the frequency of this character catching
+    # the culprit change over the year as new series are created?
     output$distPlot <- renderPlot({
        all_series <-  scoobydoo_tidy %>% 
-          filter(format == "TV Series") %>%
-          group_by(series_name, date_aired) %>%
-          count(caught_by)
+          filter(format %in% c("TV Series", "TV Series (segmented)")) %>%
+          group_by(series_name) %>%
+          count(caught_by) %>%
+          mutate(percentage = n/sum(n) * 100)
       
         highlighted_character <- all_series %>% 
           filter(caught_by == input$character_to_highlight)
+        
+        year_order <-  scoobydoo_tidy %>% 
+          filter(format %in% c("TV Series", "TV Series (segmented)")) %>%
+          group_by(series_name, date_aired) %>%
+          count(caught_by)
       
         all_series %>% 
           ggplot(aes(x = series_name,
-                     y = n,
+                     y = percentage,
                      group = caught_by),
                  srt = 35) +
           geom_line(color = "grey") +
@@ -98,7 +102,36 @@ server <- function(input, output) {
           geom_point(color = "grey")+
           geom_point(data = highlighted_character,
                      color = "red") +
-          theme(axis.text.x = element_text(angle = 45, hjust=1))
+          scale_x_discrete(name = "Series Name (in order of year)", 
+                             limits=c("Scooby Doo, Where Are You!","The New Scooby-Doo Movies",
+                                      "The Scooby-Doo Show", "Scooby-Doo and Scrappy-Doo (first series)",
+                                      "Scooby-Doo and Scrappy-Doo (second series)",
+                                      "The New Scooby and Scrappy Doo Show", "The New Scooby-Doo Mysteries",
+                                      "The 13 Ghosts of Scooby-Doo", "A Pup Named Scooby-Doo", 
+                                      "What's New Scooby-Doo?",
+                                      "Scooby-Doo Mystery Incorporated", "Warner Home Video",
+                                      "Be Cool, Scooby-Doo!", "Lego", "Scooby-Doo and Guess Who?")) +
+          theme(axis.text.x = element_text(angle = 35, hjust=.75)) +
+          labs(y = "Percentage of episodes character caught culprit",
+               title = "The Frequency at which a Character has Caught the Culprit per TV Series") +
+          theme(text = element_text(size = 15))  
+    })
+    
+    # Which motive was common throughout the franchise?
+    output$bar_plot <- renderPlot({
+      
+      scoobydoo_tidy %>% 
+        filter(motive != "NULL") %>% 
+        group_by(motive) %>% 
+        count(motive) %>% 
+        ggplot(aes(x = n,
+                   y = reorder(motive, n))) +
+        geom_col()  +
+        geom_label(aes(label = n)) +
+        labs(x = "count",
+             y = "Motives",
+             title = "Motives of the Crimes Throughout the Whole Franchise") +
+        theme(text = element_text(size = 20))  
     })
 }
 
